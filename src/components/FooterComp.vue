2adc3b984c57cf70ef36b6839e34bcaf5724d941
6atuhan <boranbatuh@gmail.com>
2023-05-30 16:44:08 +0300

    color
    header and footer color change. darkmode button disabled

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-green-200 dark:bg-green-800 w-full p-4 ">
+    <div class="bg-gradient-to-t  from-light via-light  to-transparent  w-full p-4 ">
         <h1 class="text-center">takas.shop Copyright © 2023 Istanbul - All rights reserved || Designed By: 6atuhan</h1>
     </div>
 </template>
--- src/router/views/HomePage.vue
+++ src/router/views/HomePage.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="flex items-start justify-center h-full  w-full">
-        <div class=" h-40 w-full  border-r border-b-2 hover:bg-gradient-to-r from-light via-transparent to-transparent border-dark hover:text-3xl  basis-1/2 flex items-center justify-center hover:basis-4/5 transition-all">
-            <h1 class="text-center text-dark">
-                eşyalara takas <br> <strong class="text-outline-black-thin  text-light tracking-[0.2rem]">teklifleri vermek</strong> <br> istiyorum.
+        <div class=" h-40 w-full border-r border-b-2 hover:bg-gradient-to-r from-light via-transparent to-transparent border-dark hover:text-3xl  basis-1/2 flex items-center justify-center hover:basis-4/5 transition-all">
+            <h1 class="text-center cursor-pointer text-dark text-xs md:text-lg">
+                eşyalara takas <br> <strong class="text-outline-black-thin  text-light tracking-widest md:tracking-[0.2rem]">teklifleri vermek</strong> <br> istiyorum.
             </h1>
         </div>
-        <div class=" h-40 w-full  border-l border-b-2 hover:bg-gradient-to-l from-light via-transparent to-transparent border-dark hover:text-3xl  basis-1/2 flex items-center justify-center hover:basis-4/5 transition-all">
-            <h1 class="text-center text-dark ">
-                eşyama takas <br><strong class="text-outline-black-thin  text-light tracking-[0.2rem]">teklifleri almak</strong>  <br>istiyorum.
+        <div class=" h-40 w-full border-l border-b-2 hover:bg-gradient-to-l from-light via-transparent to-transparent border-dark hover:text-3xl  basis-1/2 flex items-center justify-center hover:basis-4/5 transition-all">
+            <h1 class="text-center cursor-pointer text-dark text-xs md:text-lg ">
+                eşyama takas <br><strong class="text-outline-black-thin  text-light tracking-widest md:tracking-[0.2rem]">teklifleri almak</strong>  <br>istiyorum.
             </h1>
         </div>
     </div>
